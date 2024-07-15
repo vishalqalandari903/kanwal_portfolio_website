362189fd2a9d6b7f0a85307b7adb451f6799e9b7
Vishal Kumar <vishalfreelancing903@gmail.com>
2024-07-15 23:07:52 +0500

    hero text styles

--- src/styles.js
+++ src/styles.js
@@ -4,7 +4,7 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "font-black text-white  min-[900px]:text-[60px] min-[900px] :bg-green-500 min-[750px]:text-[50px] xs:text-[30px] min-[500px]:text-[33px] min-[400px]:text-[26px] min-[300px]:text-[22px] min-[550px]:text-[40px] text-[30px] lg:leading-[98px] mt-2 text-2vw",
+    "font-black text-white sm:text-[60px] text-[70px] lg:leading-[98px] mt-2",
   heroSubText:
     "text-[#dfd9ff] font-medium lg:text-[25px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
 
@@ -15,3 +15,4 @@ const styles = {
 };
 
 export { styles };
+// min-[900px]:text-[60px] min-[900px] min-[750px]:text-[50px] xs:text-[30px] min-[500px]:text-[33px] min-[400px]:text-[26px] min-[550px]:text-[40px] text-[50px]
